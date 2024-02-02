2136e895aafb4071cb46eb6f1d241075cfb78922
Onesmus <onesmusm@bestforu.co.za>
2024-02-02 22:01:45 +0200

    need to fix the varibles that are being printed on %v

--- rockpaperscisssor.go
+++ rockpaperscisssor.go
@@ -20,3 +20,31 @@ var actions = map[int] string{
 	Scissor: "Scissors",
 }
 
+//func is our function that  takes no arguments but returns nothing (void). It has access to all other functions and variables within this
+func main(){
+	// Println prints out the specified values and automatically appends a newline character at the end.	fmt.Println("Welcome to My Rock Paper Scissors")
+	fmt.Println("Like it says you have 3 Choices Rock Paper Scissors")
+	fmt.Println("Rock = 0\nPaper = 1\nScissors = 2")
+
+	var userInput int
+	for {
+		fmt.Print("Input")
+		_, err := fmt.Scan(&userInput)
+		if err == nil  && (userInput >= Rock && userInput <= Scissor){
+			break
+		}else{
+			fmt.Println("Invalid Input. Please enter either 0 or 1 or 2")
+		}
+
+	}
+	computerInput := rand.Intn(3) + 1 // rand.Intn returns a random number between 0 and n-1 so we add 1 to get 
+
+	var result string
+	if userInput == Rock && computerInput == Scissor{
+		result = "Congratulations! You Win!"
+	}
+
+	computerAction := actions[computerInput]
+	userAction := actions[userInput]
+	fmt.Println("%vUser's action was %vComputer's action was %v", result, userAction, computerAction)
+}
